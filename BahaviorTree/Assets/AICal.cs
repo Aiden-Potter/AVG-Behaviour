3d553b8a16937faaac6d88e2e3b137e265be4ecc
Aiden-Potter <2834691652@qq.com>
2020-06-30 16:21:24 +0800

    some new skill

---
+++ BahaviorTree/Assets/AICal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//两句命名空间的引入
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+//继承一个类
+public class AICal : Action
+{
+    public SharedInt randomNum;
+
+    public override TaskStatus OnUpdate()
+    {
+        randomNum.Value = Random.Range(1, 10);
+        return base.OnUpdate();
+    }
+}
---
+++ BahaviorTree/Assets/AIConCompare.cs
@@ -0,0 +1,22 @@
+﻿using BehaviorDesigner.Runtime.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+public class AIConCompare : Conditional
+{
+    public SharedInt int1;
+    public SharedInt int2;
+
+    public override TaskStatus OnUpdate()
+    {
+        if(int1.Value>int2.Value)
+        {
+            return TaskStatus.Success;
+        }
+        else
+        {
+            return TaskStatus.Failure;
+        }
+    }
+}
